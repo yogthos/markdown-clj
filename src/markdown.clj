5c11ced2af03efbbd1140abb43ce79435e9bf9b6
Yogthos <yogthos@gmail.com>
2011-10-11 23:22:31 -0400

    changed to handle a whole line at a time, fixed misc bugs

--- src/markdown.clj
+++ src/markdown.clj
@@ -3,286 +3,198 @@
 
 (defn- write [writer text]
   (doseq [c text] (.write writer (int c))))
-
-
-(defn- transform [writer c transformers]  
-  (let [cur-char (if (pos? c) (char c) \newline)
-        result (doall
-                 (for [transformer transformers]         
-                   (transformer writer cur-char (neg? c))))]    
-
-    (when (not (contains? #{\* \# \` \>} cur-char))
-      (write writer (str cur-char)))
-    result))
-
-
-(defn- italics-transformer 
-  ([] (italics-transformer {:italics false, :stars 0, :previous nil, :newline true}))  
-  ([{italics? :italics, stars :stars, previous :previous, newline? :newline}]    
-    (fn [writer c eof?]             
-      (italics-transformer 
-        (condp = c 
-          \*
-          {:italics italics?, :stars (inc stars), :previous c, :newline newline?}
-          
-          \newline
-          (do 
-            (when (and italics? (= stars 1))
-              (write writer "</i>"))
-            {:italics false, :stars 0, :previous c, :newline true})
  
-          ;default          
-          (cond
-            (and newline? (= previous \*) (= \space c))
-            {:italics false, :stars 0, :previous c, :newline false}
-          
-            (and (= stars 1) italics?)
-            (do
-              (write writer "</i>")
-              {:italics false, :stars 0, :previous c, :newline false})
-            
-            (= stars 1)
-            (do
-              (write writer "<i>")
-              {:italics true, :stars 0, :previous c, :newline false})
-           
-            :default
-            {:italics italics?, :stars 0, :previous c, :newline false}))))))
+(defn- init-transformer [writer & transformers]
+  (fn [line state]
+    (let [[text new-state]
+          (reduce
+            (fn [[text, state] transformer] (transformer text state))
+            [line state]           
+            transformers)]      
+      (write writer text)
+      new-state)))
+ 
+(defn- bold-transformer [text, state]
+  (if (:code state) 
+    [text, state]
+    (loop [out []
+           buf []
+           tokens (partition-by (partial = \*) (seq text))
+           cur-state (assoc state :found-token false)]
+      (cond
+        (empty? tokens)
+        [(apply str (into (if (:found-token cur-state) (into out [\* \*]) out) buf)), cur-state]
+        
+        (:found-token cur-state)
+        (if (= (first tokens) [\* \*])
+          (recur (into out (flatten [\< \b \> buf \< \/ \b \>]))
+                 []
+                 (rest tokens)
+                 (assoc cur-state :found-token false))
+          (recur out
+                 (into buf (first tokens))
+                 (rest tokens)
+                 cur-state))
+        
+        (= (first tokens) [\* \*])
+        (recur out, buf, (rest tokens), (assoc cur-state :found-token true))
+     
+        :default
+        (recur (into out (first tokens)), buf, (rest tokens), cur-state)))))
+ 
+(defn- italics-transformer [text, state]
+  (if (:code state)
+    [text, state]
+    (loop [out []
+           buf []
+           tokens (partition-by (partial = \*) (seq text))
+           cur-state (assoc state :found-token false)]
+      (cond
+        (empty? tokens)
+        [(apply str (into (if (:found-token cur-state) (conj out \*) out) buf)), cur-state]
+        
+        (:found-token cur-state)
+        (if (= (first tokens) [\*])
+          (recur (into out (flatten [\< \i \> buf \< \/ \i \>]))
+                 []
+                 (rest tokens)
+                 (assoc cur-state :found-token false))
+          (recur out
+                 (into buf (first tokens))
+                 (rest tokens)
+                 cur-state))
+     
+      (= (first tokens) [\*])
+      (recur out, buf, (rest tokens), (assoc cur-state :found-token true))
+     
+      :default
+      (recur (into out (first tokens)), buf, (rest tokens), cur-state)))))
+ 
+(defn- heading-transformer [text, state]
+  (if (:code state)
+    [text, state]
+    (let [num-hashes (count (take-while (partial = \#) (seq text)))]    
+    (if (pos? num-hashes)   
+      [(str "<h" num-hashes ">" (apply str (drop num-hashes text)) "</h" num-hashes ">"), state]
+      [text, state]))))
+
+(defn- inline-code-transformer [text, state]
+  (if (:code state)
+    [text, state]
+    (loop [out []
+           buf []
+           tokens (partition-by (partial = \`) (seq text))
+           cur-state (assoc state :found-token false)]
+      (cond
+        (empty? tokens)
+        [(apply str (into (if (:found-token cur-state) (conj out \`) out) buf)), cur-state]
+        
+        (:found-token cur-state)
+        (if (= (first tokens) [\`])
+          (recur (into out (flatten [\< \p \r \e \> buf \< \/ \p \r \e \>]))
+                 []
+                 (rest tokens)
+                 (assoc cur-state :found-token false))
+          (recur out
+                 (into buf (first tokens))
+                 (rest tokens)
+                 cur-state))
+     
+      (= (first tokens) [\`])
+      (recur out, buf, (rest tokens), (assoc cur-state :found-token true))
+     
+      :default
+      (recur (into out (first tokens)), buf, (rest tokens), cur-state)))))
+
+(defn- code-transformer [text, state]  
+  (cond 
+        
+    (:code state)
+    (if (or (:eof state) (empty? (.trim text)))
+      ["</pre>", (assoc state :code false)]      
+      [(str "\n" text), state])
+    
+    (empty? (.trim text)) 
+    [text, state]
+    
+    :default
+    (let [num-spaces (count (take-while (partial = \space) text))]
+      (if (> num-spaces 3)
+        [(str "<pre>" text), (assoc state :code true)]
+        [text, state]))))
+
+(defn- hr-transformer [text, state]
+  (if (:code state) 
+    [text, state]
+    (if (= "***" text)
+        [(str "<hr/>"), state]
+        [text, state])))
 
 
-(defn- bold-transformer 
-  ([] (bold-transformer {:bold false, :stars 0, :previous nil}))
-  ([{bold? :bold, stars :stars, previous :previous}]
-    (fn [writer c eof?]             
-      (bold-transformer 
-        (if (= \* c)           
-          {:bold bold?, :stars (inc stars)}
-                              
-          (cond
-            (and (= stars 2) bold?)
-            (do
-              (write writer "</b>")
-              {:bold false, :stars 0})
-            
-            (and (= stars 2) (not bold?))
-            (do
-              (write writer "<b>")
-              {:bold true, :stars 0})
-           
-            :default
-            {:bold bold?, :stars 0}))))))
+(defn- list-transformer [text, state]
+  (if (:code state)
+    [text, state]
+    (cond
+      (= [\* \space] (take 2 text))
+      (if (:list state)
+        [(str "<li>" (apply str (drop 2 text)) "</li>"), state]
+        [(str "<ul><li>" (apply str (drop 2 text)) "</li>"), (assoc state :list true)])
+      
+      (and (:list state) (or (:eof state) (empty? (.trim text))))
+      ["</ul>", (assoc state :list false)]
+      
+      :default
+      [text, state])))
 
+(defn- paragraph-transformer [text, state]  
+  (if (or (:code state) (:list state) (:blockquote state))
+    [text, state]
+    (cond
+      (:paragraph state)      
+      (if (or (:eof state) (empty? (.trim text)))
+        ["</p>", (assoc state :paragraph false)]
+        [text, state])
+      
+      (and (not (:eof state)) (empty? (.trim text)))
+      [(str "<p>" text), (assoc state :paragraph true)]
+      
+      :default
+      [text, state])))
 
-(defn- heading-transformer 
-  ([] (heading-transformer {:heading false, :start-of-line true, :hashes 0}))
-  ([{heading? :heading, start-of-line? :start-of-line, hashes :hashes}]
-    (fn [writer c eof?]
-      (heading-transformer
-        (condp = c
-          \#
-          {:heading heading?, :start-of-line start-of-line?, :hashes (if start-of-line?(inc hashes) 0)}
-          
-          \newline
-          (do 
-            (when heading? 
-              (write writer (str "</h" hashes ">")))
-            {:heading false, :start-of-line true, :hashes 0})
-          
-          (cond 
-            (and start-of-line? (> hashes 0) (not heading?))
-            (do 
-              (write writer (str "<h" hashes ">"))
-              {:heading true, :start-of-line false, :hashes hashes})
-            
-            :default 
-            {:heading heading?, :hashes hashes}))))))
+(defn- blockquote-transformer [text, state]
+  (if (or (:code state) (:list state))
+    [text, state]
+    (cond
+      (:blockquote state)
+      (if (or (:eof state) (empty? (.trim text)))
+        ["</p></blockquote>", (assoc state :blockquote false)]
+        [text, state])
+      
+      :default
+      (if (= \> (first text))
+        [(str "<blockquote><p>" (apply str (rest text))), (assoc state :blockquote true)]
+        [text, state]))))
 
-
-(defn- hr-transformer 
-  ([] (hr-transformer {:stars 0, :previous nil}))
-  ([{stars :stars, previous :previous}]
-    (fn [writer c eof?]
-      (hr-transformer
-        (condp = c 
-          \*
-          (if (= \* previous) 
-            {:stars (inc stars), :previous c}
-            {:stars 1, :previous c})
-          
-          \newline
-          (do            
-            (when (= stars 3)
-              (write writer "<hr/>"))
-            {:stars 0, :previous c})
-          
-          ;default
-          {:stars stars, :previous c})))))
-
-(defn- paragraph-transformer 
-  ([] (paragraph-transformer {:empty-line true, :last-line-empty true, :paragraph false, :leading-spaces 0}))
-  ([{empty-line? :empty-line, last-line-empty? :last-line-empty, paragraph? :paragraph, leading-spaces :leading-spaces}]
-    (fn [writer c eof?]
-      (paragraph-transformer
-        (condp = c
-          \newline
-          (cond             
-            (and (or eof? empty-line?) paragraph?)            
-            (do
-              (write writer "</p>")
-              {:empty-line true, :last-line-empty true, :paragraph false, :leading-spaces 0})
-            
-            empty-line?            
-            {:empty-line true, :last-line-empty true, :paragraph paragraph?, :leading-spaces 0}
-                        
-            :default
-            {:empty-line true, :last-line-empty false, :paragraph paragraph?, :leading-spaces 0})
-          
-          \space
-          {:empty-line empty-line? 
-           :last-line-empty last-line-empty? 
-           :paragraph paragraph?
-           :leading-spaces (if empty-line? (inc leading-spaces) 0)}
-          
-          (cond
-            (and last-line-empty? empty-line? (< leading-spaces 3) (not (contains? #{\> \*} c)))
-            (do
-              (write writer "<p>")
-              {:empty-line false, :last-line-empty true, :paragraph true, :leading-spaces leading-spaces})
-            
-            :default
-            {:empty-line false, :last-line-empty last-line-empty?, :paragraph paragraph?, :leading-spaces leading-spaces}))))))
-
-
-(defn- code-transformer 
-  ([] (code-transformer {:code false, :quotes 0}))
-  ([{code? :code, quotes :quotes}]
-    (fn [writer c eof?]             
-      (code-transformer 
-        (if (= \` c)           
-          {:code code?, :quotes (inc quotes)}
-                              
-          (cond
-            (and code? (= quotes 1))
-            (do
-              (write writer "</pre>")
-              {:code false, :quotes 0})
-            
-            (= quotes 1)
-            (do
-              (write writer "<pre>")
-              {:code true, :quotes 0})
-           
-            :default
-            {:code code?, :quotes 0}))))))
-
-
-(defn- indented-code-transformer 
-  ([] (indented-code-transformer {:code false, :indents 0, :empty-line true}))
-  ([{code? :code, indents :indents, empty-line? :empty-line}]
-    (fn [writer c eof?] 
-      (indented-code-transformer
-        (condp = c        
-          \newline 
-          (if (and code? (or eof? empty-line?))
-            (do
-              (write writer "</pre>")
-              {:code false, :indents 0, :empty-line true})
-            {:code code?, :indents 0, :empty-line true})
-          
-          \space
-          (if empty-line?
-            {:code code?, :indents (inc indents), :empty-line true}
-            {:code code?, :indents indents, :empty-line false})
-          
-          ;default
-          (cond 
-            (and (not code?) (> indents 3))
-            (do 
-              (write writer "<pre>    ")
-              {:code true, :indents 0, :empty-line? false})
-            
-            :default
-            {:code code?, :indents 0, :empty-line? false}))))))
-
-(defn- blockquote-transformer 
-  ([] (blockquote-transformer {:quote false, :new-line true, :empty-line true}))
-  ([{quote? :quote, new-line? :new-line, empty-line? :empty-line}]
-    (fn [writer c eof?]
-      (blockquote-transformer
-        (condp = c
-          \>
-          (if new-line?
-            (do
-              (write writer "<blockquote><p>")
-              {:quote true, :new-line new-line?, :empty-line false})
-            {:quote quote?, :new-line new-line?, :empty-line false})
-          
-          \newline
-          (if (and quote? (or eof? empty-line?))
-            (do 
-              (write writer "</p></blockquote>")
-              {:quote false, :new-line true, :empty-line true})
-            {:quote quote?, :new-line true, :empty-line true})
-          
-          {:quote quote?, :new-line false, :empty-line false})))))
-
-(defn- list-transformer 
-  ([] (list-transformer {:ul false, :first-char nil, :second-char nil, :empty-line true}))
-  ([{ul? :ul, first-char :first-char, second-char :second-char, empty-line? :empty-line}]
-    (fn [writer c eof?]
-      (list-transformer
-        (condp = c
-          \*
-          {:ul ul?, :first-char (or first-char \*), :second-char second-char, :empty-line empty-line?}
-          
-          \space
-          {:ul ul?, :first-char first-char, :second-char (or second-char \space), :empty-line empty-line?}
-          
-          \newline
-          (cond
-            (and ul? (or empty-line? eof?)) 
-            (do
-              (write writer "</li>\n</ul>")
-              {:ul false, :first-char nil, :second-char nil, :empty-line true})
-            
-            (and (not empty-line?) ul?)
-            (do
-              (write writer "</li>")
-              {:ul true, :first-char nil, :second-char nil, :empty-line true})
-                        
-            :default
-            {:ul false, :first-char nil, :second-char nil, :empty-line true})
-          
-          ;default
-          (cond
-            (and (= first-char \*) (= second-char \space) empty-line?)
-            (do
-              (write writer (if ul? "<li>" "<ul>\n<li>"))
-              {:ul true, :first-char first-char :second-char second-char, :empty-line false})
-            
-            :default
-            {:ul ul?, :first-char first-char :second-char second-char, :empty-line false}))))))
-
-;;TODO links, nested lists
 
 (defn markdown-to-html 
   "reads markdown content from the input stream and writes HTML to the provided output stream"
   [in out]    
   (with-open [reader (new BufferedReader in)
               writer (new BufferedWriter out)]    
-    (loop [c (.read reader)
-           transformers [(italics-transformer)
-                         (bold-transformer)
-                         (heading-transformer)
-                         (hr-transformer)
-                         (blockquote-transformer)
-                         (paragraph-transformer)
-                         (code-transformer)
-                         (indented-code-transformer)
-                         (list-transformer)]]
-      
-      (let [state (transform writer c transformers)] 
-        (when (> c 0)        
-          (recur
-            (.read reader) state))))
+    (let [transformer (init-transformer 
+                        writer
+                        inline-code-transformer
+                        code-transformer
+                        hr-transformer
+                        heading-transformer
+                        italics-transformer
+                        bold-transformer
+                        list-transformer
+                        blockquote-transformer
+                        paragraph-transformer)] 
+      (loop [line (.readLine reader)
+             state {}]              
+      (if line        
+        (recur (.readLine reader) (transformer line state))
+        (transformer "" (assoc state :eof true)))))
     (.flush writer)))
--- test/mdtests.clj
+++ test/mdtests.clj
@@ -9,50 +9,51 @@
     (markdown/markdown-to-html input output)
     (.toString output)))
 
+
 (deftest heading1 
-  (is (= "<p><h1>foo</h1></p>\n" (convert "#foo"))))
+  (is (= "<h1>foo</h1>" (convert "#foo"))))
 
 (deftest heading2 
-  (is (= "<p><h2>foo</h2></p>\n" (convert "##foo"))))
+  (is (= "<h2>foo</h2>" (convert "##foo"))))
 
 (deftest hr
-  (is (= "<hr/>\n" (convert "***"))))
+  (is (= "<hr/>" (convert "***"))))
 
 (deftest italics
-  (is (= "<i>foo</i>\n" (convert "*foo*"))))
+  (is (= "<i>foo</i>" (convert "*foo*"))))
 
 (deftest bold
-  (is (= "<b>foo</b>\n" (convert "**foo**"))))
+  (is (= "<b>foo</b>" (convert "**foo**"))))
 
 (deftest bold-inside-italics
-  (is (= "<i>foo<b>bar</b>baz</i>\n" (convert "*foo**bar**baz*"))))
+  (is (= "<i>foo<b>bar</b>baz</i>" (convert "*foo**bar**baz*"))))
 
 (deftest italics-inside-bold
-  (is (= "<b>foo<i>bar</i>baz</b>\n" (convert "**foo*bar*baz**"))))
+  (is (= "<b>foo<i>bar</i>baz</b>" (convert "**foo*bar*baz**"))))
 
 (deftest paragraph
-  (is (= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>\n"
-         (convert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"))))
+  (is (= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>"
+         (convert "\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore"))))
 
 (deftest paragraph-multiline
-  (is (= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation\nullamco laboris nisi ut aliquip ex ea commodo consequat</p>\n"
-         (convert "Lorem ipsum dolor sit amet, consectetur adipisicing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation\nullamco laboris nisi ut aliquip ex ea commodo consequat"))))
+  (is (= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>"
+         (convert "\nLorem ipsum dolor sit amet, consectetur adipisicing elit, \nsed do eiusmod tempor incididunt ut labore"))))
+
 
 (deftest mulitple-paragraphs
-  (is (= "<p>foo bar baz\n</p>\n<p>foo bar baz</p>\n" 
-      (convert "foo bar baz\n\nfoo bar baz"))))
+  (is (= "<p>foo bar baz</p><p>foo bar baz</p>" 
+      (convert "\nfoo bar baz\n\n\nfoo bar baz"))))
 
 (deftest ul
-  (is (= " <ul>\n<li>foo</li>\n <li>bar</li>\n <li>baz</li>\n</ul>\n" 
+  (is (= "<ul><li>foo</li><li>bar</li><li>baz</li></ul>" 
          (convert "* foo\n* bar\n* baz"))))
 
 (deftest code
-  (is (= "<p>foo bar baz <pre>x = y + z;</pre> foo</p>\n"
+  (is (= "foo bar baz <pre>x = y + z;</pre> foo"
          (convert "foo bar baz `x = y + z;` foo"))))
 
 (deftest multiline code
-  (is (= "\n    <pre>    x = 5\n    y = 6\n    z = x + y</pre>\n"
-         (convert "\n    x = 5\n    y = 6\n    z = x + y")))) 
+  (is (= "<pre>    x = 5\n    y = 6\n    z = x + y</pre>"
+         (convert "    x = 5\n    y = 6\n    z = x + y")))) 
 
-(run-tests 'mdtests)
-
+(run-tests)
