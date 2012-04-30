93eb8fc8129c1e7c5c8997cc75a2da94661b7f6e
Yogthos <dmitri.sotnikov@gmail.com>
2012-04-30 19:47:27 -0400

    updated to produce optimized js

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject markdown-clj "0.7"
-    :description "Markdown parser"
-    :dependencies [[org.clojure/clojure "1.4.0"]]
-    :plugins [[lein-cljsbuild "0.1.8"]]
-    
-    :cljsbuild
-    {:crossovers [transformers],
-     :builds
-     [{:source-path "src-cljs",
-       :crossover-path src-cljs,
-       :optimizations :advanced
-       :compiler {:output-to "js/markdown.js"}}]})
-
+  :description "Markdown parser"
+   :dependencies [[org.clojure/clojure "1.4.0"]]
+   :plugins [[lein-cljsbuild "0.1.8"]]
+   
+   :cljsbuild
+   {:crossovers [transformers],
+    :builds
+    [{:source-path "src-cljs",
+      :crossover-path src-cljs,
+      :compiler {:output-to "js/markdown.js"
+                 :optimizations :advanced
+                 :pretty-print false}}]})
