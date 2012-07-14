6a6ee3de0caabc1b9ba188e0c1427111946f72a0
Yogthos <dmitri.sotnikov@gmail.com>
2012-07-14 19:08:23 -0300

    Update master

--- test/mdtests.clj
+++ test/mdtests.clj
@@ -15,22 +15,22 @@
   (is (= "<hr/>" (markdown/md-to-html-string "- - - "))))
 
 (deftest em
-  (is (= "<em>foo</em>" (markdown/md-to-html-string "*foo*"))))
+  (is (= "<p><em>foo</em></p>" (markdown/md-to-html-string "*foo*"))))
 
 (deftest italics
-  (is (= "<i>foo</i>" (markdown/md-to-html-string "_foo_"))))
+  (is (= "<p><i>foo</i></p>" (markdown/md-to-html-string "_foo_"))))
 
 (deftest bold
-  (is (= "<b>foo</b>" (markdown/md-to-html-string "**foo**"))))
+  (is (= "<p><b>foo</b></p>" (markdown/md-to-html-string "**foo**"))))
 
 (deftest alt-bold
-  (is (= "<b>foo</b>" (markdown/md-to-html-string "__foo__"))))
+  (is (= "<p><b>foo</b></p>" (markdown/md-to-html-string "__foo__"))))
 
 (deftest bold-inside-em
-  (is (= "<em>foo<b>bar</b>baz</em>" (markdown/md-to-html-string "*foo**bar**baz*"))))
+  (is (= "<p><em>foo<b>bar</b>baz</em></p>" (markdown/md-to-html-string "*foo**bar**baz*"))))
 
 (deftest em-inside-bold
-  (is (= "<b>foo<em>bar</em>baz</b>" (markdown/md-to-html-string "**foo*bar*baz**"))))
+  (is (= "<p><b>foo<em>bar</em>baz</b></p>" (markdown/md-to-html-string "**foo*bar*baz**"))))
 
 (deftest paragraph
   (is (= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore</p>"
@@ -45,28 +45,28 @@
       (markdown/md-to-html-string "\nfoo bar baz\n\n\nfoo bar baz"))))
 
 (deftest ul
-  (is (= "<ul><li>foo</li><li>bar</li><li>baz</li></ul>" 
+  (is (= "<p><ul><li>foo</li><li>bar</li><li>baz</li></ul></p>" 
          (markdown/md-to-html-string "* foo\n* bar\n* baz"))))
 
 (deftest ul-nested
-  (is (= "<ul><li>first item<ul><li>first sub-item<ul><li>second sub-item</li></ul></li><li>third sub-item</li></ul></li><li>second item<ul><li>first sub-item</li><li>second sub-item</li></ul></li><li>third item</li></ul>" 
+  (is (= "<p><ul><li>first item<ul><li>first sub-item<ul><li>second sub-item</li></ul></li><li>third sub-item</li></ul></li><li>second item<ul><li>first sub-item</li><li>second sub-item</li></ul></li><li>third item</li></ul></p>" 
          (markdown/md-to-html-string "* first item\n * first sub-item\n  * second sub-item\n * third sub-item\n* second item\n * first sub-item\n * second sub-item\n* third item"))))
 
 (deftest ol
-  (is (= "<ol><li> Foo</li><li> Bar</li><li> Baz</li></ol>"
+  (is (= "<p><ol><li> Foo</li><li> Bar</li><li> Baz</li></ol></p>"
          (markdown/md-to-html-string "1. Foo\n2. Bar\n3. Baz"))))
 
 (deftest ul-in-ol
-  (is (= "<ol><li> Bar<ol><li> Subbar<ul><li>foo</li><li>bar</li><li>baz</li></ul><li> Baz</li></ol></li></ol>"
-      (markdown/md-to-html-string "1. Bar\n 2. Subbar\n  * foo\n  * bar\n  * baz\n3. Baz"))))
+  (is (= "<p><ol><li> Bar<ol><li> Subbar<ul><li>foo</li><li>bar</li><li>baz</li></ul><li> Baz</li></ol></li></ol></p>"         
+         (markdown/md-to-html-string "1. Bar\n 2. Subbar\n  * foo\n  * bar\n  * baz\n3. Baz"))))
 
 (deftest ol-in-ul
-  (is (= "<ul><li>Foo<ol><li> Bar<ol><li> Subbar</li></ol></li></ol><li>Baz</li></ul>" 
+  (is (= "<p><ul><li>Foo<ol><li> Bar<ol><li> Subbar</li></ol></li></ol><li>Baz</li></ul></p>" 
          (markdown/md-to-html-string "* Foo\n 1. Bar\n  1. Subbar\n* Baz"))))
 
 
 (deftest code
-  (is (= "foo bar baz <code>x = y + z;</code> foo"
+  (is (= "<p>foo bar baz <code>x = y + z;</code> foo</p>"
          (markdown/md-to-html-string "foo bar baz `x = y + z;` foo"))))
 
 (deftest multiline-code
@@ -78,30 +78,28 @@
          (markdown/md-to-html-string "```(defn- write [writer text]\n  (doseq [c text]\n    (.write writer (int c))))\n```"))))
 
 (deftest stirkethrough
-  (is (= "<del>foo</del>"
+  (is (= "<p><del>foo</del></p>"
          (markdown/md-to-html-string "~~foo~~"))))
 
 (deftest superscript
-  (is (= "foo<sup>bar</sup> baz"
+  (is (= "<p>foo<sup>bar</sup> baz</p>"
          (markdown/md-to-html-string "foo^bar baz"))))
 
 (deftest link
-  (is (= "<a href='http://github.com'>github</a>"
+  (is (= "<p><a href='http://github.com'>github</a></p>"
          (markdown/md-to-html-string "[github](http://github.com)"))))
 
 (deftest img
-  (is (= "<a href='/path/to/img.jpg'>Alt text</a> <img src=\"/path/to/img.jpg\" alt=\"Alt text\" />"
-         (markdown/md-to-html-string "[Alt text](/path/to/img.jpg) ![Alt text](/path/to/img.jpg)"))))
+  (is (= "<p><img src=\"/path/to/img.jpg\" alt=\"Alt text\" /></p>" 
+         (markdown/md-to-html-string "![Alt text](/path/to/img.jpg)"))))
 
 (deftest bad-link
-  (is (= "[github](http://github.comfooo" 
+  (is (= "<p>[github](http://github.comfooo</p>" 
          (markdown/md-to-html-string "[github](http://github.comfooo"))))
 
 (deftest bad-link-title
-  (is (= "[github(http://github.comfooo)" 
+  (is (= "<p>[github(http://github.comfooo)</p>" 
          (markdown/md-to-html-string "[github(http://github.comfooo)"))))
 
-
 ;(run-tests)
-
 
