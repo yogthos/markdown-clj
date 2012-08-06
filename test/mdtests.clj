184ce075a0af0bf1eca2378ec0b8c843b6ee7e07
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-06 11:53:09 -0400

    added html escaping in code

--- test/mdtests.clj
+++ test/mdtests.clj
@@ -68,6 +68,8 @@
 (deftest code
   (is (= "<p>foo bar baz <code>x = y + z;</code> foo</p>"
          (markdown/md-to-html-string "foo bar baz `x = y + z;` foo")))
+  (is (= "<p><code>&lt;?xml version='1.0' encoding='UTF-8'?&gt;&lt;channel&gt;&lt;&frasl;channel&gt;</code></p>"
+         (markdown/md-to-html-string "`<?xml version='1.0' encoding='UTF-8'?><channel></channel>`")))
   (is (= "<p>foo bar baz <code>&#40;fn &#91;x &amp; xs&#93; &#40;str &quot;x:&quot; x&#41;&#41;</code> foo</p>"
          (markdown/md-to-html-string "foo bar baz `(fn [x & xs] (str \"x:\" x))` foo"))))
 
