3ff934a1c028ca99875ea3df1c229711c9c2fca4
Yogthos <dmitri.sotnikov@gmail.com>
2013-04-21 03:08:45 -0400

    added benchmarks

--- project.clj
+++ project.clj
@@ -3,13 +3,18 @@
    :url "https://github.com/yogthos/markdown-clj"
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :dependencies [[org.clojure/clojure "1.4.0"]]
-   :plugins [[lein-cljsbuild "0.2.9"]]
+   :dependencies [[org.clojure/clojure "1.5.1"]
+                  [criterium "0.3.1" :scope "test"]]
+   :plugins [[lein-cljsbuild "0.3.0"]]
+   
+   :test-selectors {:default (complement :benchmark)
+                    :benchmark :benchmark
+                    :all (constantly true)}
    
    :cljsbuild
    {:crossovers [markdown.transformers],
     :builds
-    [{:source-path "src-cljs",      
+    [{:source-paths ["src-cljs"],      
       :compiler {:output-to "js/markdown.js"
                  :optimizations :advanced
                  :pretty-print false}}]})
