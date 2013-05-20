3a7682dce14f57e645a74905657e521a45c18824
Yogthos <dmitri.sotnikov@gmail.com>
2013-05-20 13:04:32 -0400

    updated for cljs

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject markdown-clj "0.9.22"
+(defproject markdown-clj "0.9.23"
+  :clojurescript? true
   :description "Markdown parser"
    :url "https://github.com/yogthos/markdown-clj"
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
    :dependencies [[org.clojure/clojure "1.5.1"]
                   [criterium "0.3.1" :scope "test"]]
-   :plugins [[lein-cljsbuild "0.2.9"]]
-   
+   :plugins [[lein-cljsbuild "0.3.2"]]
+   :hooks [leiningen.cljsbuild]
    :test-selectors {:default (complement :benchmark)
                     :benchmark :benchmark
                     :all (constantly true)}
    
    :cljsbuild
-   {:crossovers [markdown.transformers],
+   {:crossovers [markdown.transformers]
     :builds
-    [{:source-paths ["src-cljs"],      
-      :compiler {:output-to "js/markdown.js"
-                 :optimizations :advanced
+    [{:source-paths ["src-cljs"]      
+      :compiler {:optimizations :advanced
                  :pretty-print false}}]})
