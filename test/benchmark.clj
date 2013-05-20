f22e93a060687afcc9a6b657102c529a5b09ce86
Yogthos <dmitri.sotnikov@gmail.com>
2013-05-20 11:13:28 -0400

    added benchmark test

---
+++ test/benchmark.clj
@@ -0,0 +1,12 @@
+(ns benchmark
+  (:require [clojure.test :refer :all]
+            [markdown.core :as markdown]
+            [criterium.core :as criterium]))
+
+
+
+(deftest ^:benchmark bench []
+  (criterium/bench
+    (markdown/md-to-html-string 
+      "\nLorem ipsum **dolor** sit amet, consectetur _adipisicing elit_, sed do eiu^smod tem^por incididunt ut labore")))
+
