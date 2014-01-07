875f2ff3cc0d5381f61d9deeca8dd18ed65e72dc
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-01-07 16:08:50 -0500

    added an explicit cljs dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
    :dependencies [[org.clojure/clojure "1.5.1"]
+                  [org.clojure/clojurescript "0.0-2014"]
                   [criterium "0.3.1" :scope "test"]]
    :plugins [[lein-cljsbuild "1.0.1"]]
    :hooks [leiningen.cljsbuild]
