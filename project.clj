ced8009c6f15644715ad9f3d1767dacf844a9d7d
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-03-25 13:27:37 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
    :url "https://github.com/yogthos/markdown-clj"
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :dependencies [[org.clojure/clojure "1.5.1"]
+   :dependencies [[org.clojure/clojure "1.6.0"]
                   [org.clojure/clojurescript "0.0-2127"]
                   [criterium "0.4.3" :scope "test"]
                   [commons-lang "2.6" :scope "test"]]
