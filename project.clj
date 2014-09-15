ac9fe65a3d8a566484597d68dcff56d3f7d8f780
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-09-15 17:29:15 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,12 +4,8 @@
    :url "https://github.com/yogthos/markdown-clj"
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [org.clojure/clojurescript "0.0-2322"]
-                  [criterium "0.4.3" :scope "test"]
-                  [commons-lang "2.6" :scope "test"]]
+   :dependencies [[org.clojure/clojure "1.6.0"]]
 
-   :plugins [[lein-cljsbuild "0.3.2"]]
    :hooks [leiningen.cljsbuild]
    :test-selectors {:default (complement :benchmark)
                     :benchmark :benchmark
@@ -29,4 +25,11 @@
              {:compiler {:optimizations :whitespace
                          :pretty-print true}}}}
   :profiles
-  {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :dev
+        {:dependencies [[criterium "0.4.3" :scope "test"]
+                        [commons-lang "2.6" :scope "test"]
+                        [org.clojure/clojurescript "0.0-2322"]]
+         :plugins [[lein-cljsbuild "1.0.3"]]}})
+
+
