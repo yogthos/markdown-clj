253c3b40f8eb7807ee9a99c22fed1efb903f7d52
Yogthos <dmitri.sotnikov@gmail.com>
2014-09-15 18:19:59 -0400

    fixed running tests

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
              :url "http://www.eclipse.org/legal/epl-v10.html"}
    :dependencies [[org.clojure/clojure "1.6.0"]]
 
-   :hooks [leiningen.cljsbuild]
    :test-selectors {:default (complement :benchmark)
                     :benchmark :benchmark
                     :all (constantly true)}
