84a535b7705c7d5d233ca604c2fbc53fcf232b80
Yogthos <dmitri.sotnikov@gmail.com>
2014-09-26 13:36:57 -0400

    updated to use cljx

--- project.clj
+++ project.clj
@@ -5,17 +5,15 @@
              :url "http://www.eclipse.org/legal/epl-v10.html"}
    :dependencies [[org.clojure/clojure "1.6.0"]]
    :clojurescript? true
-   :source-paths ["src" "src-cljs"]
+   :source-paths ["src" "src-cljs" "src-cljx" "target/generated/cljs"]
+   :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
    :test-selectors {:default (complement :benchmark)
                     :benchmark :benchmark
                     :all (constantly true)}
-   :cljsbuild
-   {:crossovers [markdown.transformers]
-    :crossover-path "crossover"
-    :crossover-jar true
 
-    :builds {:main
-             {:source-paths ["src-cljs"]
+   :cljsbuild
+   {:builds {:main
+             {:source-paths ["src-cljs" "target/generated/cljs"]
               :jar true
               :compiler {:output-to "js/markdown.js"
                          :optimizations :advanced
@@ -29,6 +27,16 @@
         {:dependencies [[criterium "0.4.3" :scope "test"]
                         [commons-lang "2.6" :scope "test"]
                         [org.clojure/clojurescript "0.0-2322"]]
-         :plugins [[lein-cljsbuild "1.0.3"]]}})
+         :plugins [[lein-cljsbuild "1.0.3"]
+                   [com.keminglabs/cljx "0.4.0"]]
+         :cljx {:builds [{:source-paths ["src-cljx"]
+                          :output-path "target/generated/cljs"
+                          :rules :clj}
+                         {:source-paths ["src-cljx"]
+                          :output-path "target/generated/cljs"
+                          :rules :cljs}]}
+         :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
+                   "install" ["do" "clean," "cljx" "once," "install"]
+                   "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}}})
 
 
