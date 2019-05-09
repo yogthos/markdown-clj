52557afa146f8ea35f9c9e6b20a9c39145044900
Christopher Small <metasoarous@gmail.com>
2019-05-09 15:20:22 -0700

    Add yaml and edn metadata processing; solves #153 (#154)
    * allow for multiple lang/class specifications on code blocks; fixes #147
    
    * code block tests passing
    
    * Added yaml and edn metadata handling

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/yogthos/markdown-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.forward/yaml "1.0.9"]]
   :clojurescript? true
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
   :test-selectors {:default   (complement :benchmark)
---
+++ test/files/metadata-yaml.md
@@ -0,0 +1,12 @@
+---
+title: My Document
+summary: A brief description of my document.
+authors: 
+  - Justin May
+  - Spooky Mulder
+  - End Line At End  
+date: October 31, 2015
+base_url: http://example.com
+---
+
+# The Document
