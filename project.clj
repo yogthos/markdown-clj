9abd0436959b8bb476bc90cc820d96b4156cfcfc
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-11-17 09:58:31 -0500

    switch to use bb for tests

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject markdown-clj "1.10.6"
+(defproject markdown-clj "1.10.7"
   :description "Markdown parser"
   :url "https://github.com/yogthos/markdown-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-commons/clj-yaml "0.7.107"]]
   :clojurescript? true
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
@@ -12,40 +12,16 @@
                    :all       (constantly true)}
   :auto-clean false
 
-   :aliases {"test-cljs" ["doo" "rhino" "test" "once"]
+   :aliases {"test-cljs" ["shell" "bb" "test:cljs"]
              "test"      ["do" "test," "test-cljs"]
              "cleantest" ["do" "clean," "test"]
              "install"   ["do" "clean," "install"]
              "deploy"    ["do" "clean," "deploy" "clojars"]}
 
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
-  :cljsbuild
-  {:builds {:main
-            {:source-paths ["src/cljc" "src/cljs"]
-             :jar          true
-             :compiler     {:output-to     "demo/js/markdown.js"
-                            :optimizations :advanced
-                            :pretty-print  false}}
-            :dev
-            {:compiler {:optimizations :whitespace
-                        :pretty-print  true}}
-
-            :test
-            {:source-paths ["src/cljc" "src/cljs" "test"]
-             :compiler {:output-to "target/unit-test.js"
-                        :output-dir "target"
-                        :main markdown.runner
-                        :optimizations :whitespace}}}}
   :profiles
-  {:demo
-   {}
-   :dev
+  {:dev
    {:jvm-opts ["-XX:-TieredCompilation"]
-    :dependencies [[criterium "0.4.4" :scope "test"]
-                   [commons-lang "2.6" :scope "test"]
-                   [org.clojure/clojurescript "1.10.339"]
-                   [org.mozilla/rhino "1.7.10"]]
-    :plugins      [[lein-cljsbuild "1.1.7"]
-                   [lein-doo "0.1.10"]]}}
-  :doo {:build "test"
-        :paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}})
+    :plugins [[lein-shell "0.4.1"]]
+    :dependencies [[criterium "0.4.6" :scope "test"]
+                   [commons-lang "2.6" :scope "test"]]}})
