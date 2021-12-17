ac245d3049afa25a6d41fcb5ba5a268f52c610e4
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-12-17 10:40:42 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject markdown-clj "1.10.7"
+(defproject markdown-clj "1.10.8"
   :description "Markdown parser"
   :url "https://github.com/yogthos/markdown-clj"
   :license {:name "Eclipse Public License"
