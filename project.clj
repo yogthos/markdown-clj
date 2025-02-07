e8eab646f8a6e232431be80c985f338a4537ef4e
U-C-S <uchanakyasrinivas@gmail.com>
2025-02-07 17:45:46 +0530

    Update `clj-yaml` to `1.0.29`
    Fixes CVE warning for snakeyaml 1.33

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clj-commons/clj-yaml "1.0.26"]]
+                 [clj-commons/clj-yaml "1.0.29"]]
   :clojurescript? true
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
   :test-selectors {:default   (complement :benchmark)
@@ -12,16 +12,16 @@
                    :all       (constantly true)}
   :auto-clean false
 
-   :aliases {"test-cljs" ["shell" "bb" "test:cljs"]
-             "test-bb"   ["shell" "bb" "test:bb"]
-             "test-nbb"  ["shell" "bb" "test:nbb"]
-             "test"      ["do" "test," "test-cljs", "test-bb", "test-nbb"]
-             "cleantest" ["do" "clean," "test"]
-             "install"   ["do" "clean," "install"]
-             "deploy"    ["do" "clean," "deploy" "clojars"]}
+  :aliases {"test-cljs" ["shell" "bb" "test:cljs"]
+            "test-bb"   ["shell" "bb" "test:bb"]
+            "test-nbb"  ["shell" "bb" "test:nbb"]
+            "test"      ["do" "test," "test-cljs", "test-bb", "test-nbb"]
+            "cleantest" ["do" "clean," "test"]
+            "install"   ["do" "clean," "install"]
+            "deploy"    ["do" "clean," "deploy" "clojars"]}
 
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
-   
+
   :profiles
   {:dev
    {:jvm-opts ["-XX:-TieredCompilation"]
